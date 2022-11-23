e77f55479d5d43f8840b8721ff9c372b92d69693
artem-from-spb <96229835+artem-from-spb@users.noreply.github.com>
2022-11-23 21:07:26 +0400

    fix: readme

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__text">© 2022 Mesto Russiaфффф</p>
+      <p className="footer__text">© 2022 Mesto Russia</p>
     </footer>
   );
 }
